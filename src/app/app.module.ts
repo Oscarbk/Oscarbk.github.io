e5bf74905874bb00a6a7c484fa2b1cf77945f1a3
Oscarbk <oscarbk@gwu.edu>
2021-07-24 23:19:27 -0500

    Made non-featured blog posts render dynamically

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './pages/header/header.component';
 import { FooterComponent } from './pages/footer/footer.component';
+import { BlogPostComponent } from './pages/blog-post/blog-post.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    BlogPostComponent
   ],
   imports: [
     BrowserModule,
