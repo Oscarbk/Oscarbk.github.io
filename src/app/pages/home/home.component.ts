6be2547cbc01062a9a411f3b133755d5d2797ca0
Oscarbk <oscarbk@gwu.edu>
2021-08-13 17:45:52 -0500

    Added shpe event to blog posts

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -21,11 +21,11 @@ export class HomeComponent implements OnInit {
       body: "SHPE's National Convention is the largest gathering of Hispanic STEM professionals and students. This year, it will take place in Orlando, Florida. You won't want to miss this amazing opportunity!",
       link: "https://docs.google.com/forms/d/e/1FAIpQLSc84VHawV88LodE-M8FK-F9UuOAqUxfJm4zxAtiNqadYyee-A/viewform"
     }, {
-      imageUrl: "https://dummyimage.com/700x350/dee2e6/6c757d.jpg",
-      date: "August 27",
-      title: "Register for thing",
-      body: "Register for the upcoming event that is coming",
-      link: ""
+      imageUrl: "assets/images/mentor banner.png",
+      date: "August 8",
+      title: "GWU MentorSHPE Program",
+      body: "You can sign up to become a mentor and help a mentee navigate through their college classes, possible internships, and support their growth and development.",
+      link: "https://docs.google.com/forms/d/e/1FAIpQLSfB2BaFT_nKwb7KkR-i7w4zp1p_4ukBbBFiZKRpCxC_WlkAqA/viewform"
     }, {
       imageUrl: "https://dummyimage.com/700x350/dee2e6/6c757d.jpg",
       date: "December 12",
