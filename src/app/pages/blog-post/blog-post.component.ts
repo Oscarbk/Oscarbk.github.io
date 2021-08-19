5c85d2dd68a4b60b7750124aaa465e2cba2f4c1f
Oscar Becerra <oscarbk@gwu.edu>
2021-08-19 16:36:32 -0500

    Changed css color scheme to match GW's

--- src/app/pages/blog-post/blog-post.component.ts
+++ src/app/pages/blog-post/blog-post.component.ts
@@ -4,6 +4,7 @@ import {BlogPost} from "../../modules/BlogPost";
 @Component({
   selector: 'app-blog-post',
   templateUrl: './blog-post.component.html',
+  styleUrls: ['./blog-post.component.css']
 })
 export class BlogPostComponent implements OnInit {
   @Input() blogPost!: BlogPost;
