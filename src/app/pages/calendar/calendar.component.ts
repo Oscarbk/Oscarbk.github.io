9d0e752b38ca6eda3aa21bd284c551db99ab55b3
Oscar Becerra <oscarbk@gwu.edu>
2021-08-19 17:09:47 -0500

    Added semester events to calendar

--- src/app/pages/calendar/calendar.component.ts
+++ src/app/pages/calendar/calendar.component.ts
@@ -10,18 +10,60 @@ export class CalendarComponent implements OnInit {
   Events = [
     {
       title: "Classes Start",
-      start: "2021-08-27"
+      start: "2021-08-30"
     },
     {
-      title: "Orientation",
-      start: "2021-08-27",
-      color: "red"
+      title: "Labor Day",
+      start: "2021-09-06"
     },
     {
-      title: "SHPE National Convention",
-      start: "2021-11-10",
-      end: "2021-11-14"
-    }
+      title: "Fall Break",
+      start: "2021-10-22"
+    },
+    {
+      title: "Thanksgiving Break",
+      start: "2021-11-24",
+      end: "2021-11-27"
+    },
+    {
+      title: "Last Day of Classes",
+      start: "2021-12-11"
+    },
+    {
+      title: "Welcome/Intro Meeting",
+      start: "2021-09-07",
+      end: "2021-09-10",
+      color: "DarkGoldenRod"
+    },
+    {
+      title: "Social Night w GW Fuego",
+      start: "2021-09-24",
+      color: "Crimson"
+    },
+    {
+      title: "Resume Workshop",
+      start: "2021-10-04",
+      end: "2021-10-08",
+      color: "DarkGoldenRod"
+    },
+    {
+      title: "Interview Prep",
+      start: "2021-10-18",
+      end: "2021-10-22",
+      color: "DarkGoldenRod"
+    },
+    {
+      title: "Social Event (TBD)",
+      start: "2021-11-01",
+      end: "2021-11-05",
+      color: "Crimson"
+    },
+    {
+      title: "GWU SHPE Elections",
+      start: "2021-11-29",
+      end: "2021-12-03",
+      color: "DarkGoldenRod"
+    },
   ];
 
   constructor() { }
@@ -39,7 +81,7 @@ export class CalendarComponent implements OnInit {
         //dateClick: this.onDateClick.bind(this),
         events: this.Events
       };
-    }, 250);
+    }, 400);
 
   }
 
